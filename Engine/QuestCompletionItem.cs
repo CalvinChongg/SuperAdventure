e74a910fa28c79b3b719344cac761309ebcc860c
iconicstixx <ccalvinoutlookcom@gmail.com>
2024-04-26 21:50:35 +0800

    Testing

---
+++ Engine/QuestCompletionItem.cs
@@ -0,0 +1,21 @@
+﻿using Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperAdventure
+{
+    public class QuestCompletionItem
+    {
+        public Item Details { get; set; }
+        public int Quantity { get; set; }
+        public QuestCompletionItem (Item details, int quantity )
+        {
+            Details = details;
+            Quantity = quantity;
+        }
+    }
+}
