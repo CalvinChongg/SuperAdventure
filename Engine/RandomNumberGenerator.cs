219cdc205c033b100383037fc3d5a0aaff22112c
iconicstixx <ccalvinoutlookcom@gmail.com>
2024-04-30 13:03:13 +0800

    refactor: random number generator

--- Engine/RandomNumberGenerator.cs
+++ Engine/RandomNumberGenerator.cs
@@ -8,5 +8,11 @@ namespace Engine
 {
     internal class RandomNumberGenerator
     {
+        private static Random _generator = new Random();
+
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            return _generator.Next(minimumValue, maximumValue + 1)
+        }
     }
 }
